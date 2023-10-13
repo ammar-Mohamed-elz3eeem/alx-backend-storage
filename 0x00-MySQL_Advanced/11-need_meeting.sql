20e1b28e3526d87ce3a264535699a0b12cb7d0fb
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-13 18:48:02 +0200

    add view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,3 @@
+-- create view named name_meeting
+
+CREATE OR REPLACE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), interval 1 MONTH));
