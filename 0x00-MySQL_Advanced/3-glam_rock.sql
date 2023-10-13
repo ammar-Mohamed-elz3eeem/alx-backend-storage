958ce8d5f99abe916c0c55362cd65cb67ed07537
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-13 04:37:45 +0200

    change date to 2020

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- get longlived Gram lock style bands
 
-SELECT DISTINCT band_name, (IFNULL(2022, 'split') - formed) AS lifespan 
+SELECT DISTINCT band_name, IFNULL(2020, 'split') - formed AS lifespan 
 	FROM metal_bands
 	WHERE FIND_IN_SET('Glam rock', style)
 	ORDER BY lifespan DESC;
