fbe77af82f86602a7cea4018da3e74b4292e07e1
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-13 04:36:02 +0200

    add glam rock classifier

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- get longlived Gram lock style bands
+
+SELECT band_name, (IFNULL(2022, 'split') - formed) AS lifespan 
+	FROM metal_bands
+	WHERE FIND_IN_SET('Glam rock', style)
+	ORDER BY lifespan DESC;
