703726f859260e55465068bc78d31b14cc21e3ef
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-13 17:04:11 +0200

    add SafeDiv functionality

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- create function SafeDiv that divides two numbers and return the result
+-- and if the domenitaor is 0 then returns 0
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	END IF;
+	RETURN (a / b);
+END $$
+
+DELIMITER ;
