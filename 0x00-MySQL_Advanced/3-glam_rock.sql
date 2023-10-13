b3f95f0895965a02f12f5c900b0a28e5657c67c7
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-13 04:43:38 +0200

    fix mysql errors

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 -- get longlived Gram lock style bands
 
-SELECT DISTINCT band_name, 
-	IFNULL(2022, 'split') - formed AS lifespan 
-	FROM metal_bands
-	WHERE FIND_IN_SET('Glam rock', style)
-	ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
