2f0f54489b5d669eb2f89fd6de14dd0998dc5270
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-21 06:01:39 +0200

    add insert & read mongo collection using python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""script to list all documents in collection
+"""
+from pymongo.collation import Collation
+from pymongo.cursor import CursorType
+
+
+def list_all(mongo_collection: Collation) -> CursorType:
+    """get all documents defined in single collection
+    with collection name as input
+
+    Args:
+        mongo_collection (MongoClient.Database.Collection): mongo collection to get its documents
+
+    Returns:
+        List[Dict[str, str]]: all documents inside given collection
+    """
+    return mongo_collection.find()
---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""script to insert in collection
+"""
+from typing import Dict, Any
+from pymongo.collation import Collation
+
+
+def insert_school(mongo_collection: Collation, **kwargs: Dict[Any, Any]) -> str:
+    """insert document into collection given as input
+
+    Args:
+        mongo_collection (Collection): mongo collection to insert in it
+
+    Returns:
+        str: newely inserted item id
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id.__str__()
