e5064825d42e1c3dd0b57e42a6d1313312cbf68f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-10-21 08:20:49 +0300

    update cursortype with cursor

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,15 +2,16 @@
 """script to list all documents in collection
 """
 from pymongo.collection import Collection
-from pymongo.cursor import CursorType
+from pymongo.cursor import Cursor
 
 
-def list_all(mongo_collection: Collection) -> CursorType:
+def list_all(mongo_collection: Collection) -> Cursor:
     """get all documents defined in single collection
     with collection name as input
 
     Args:
-        mongo_collection (MongoClient.Database.Collection): mongo collection to get its documents
+        mongo_collection (MongoClient.Database.Collection):
+        mongo collection to get its documents
 
     Returns:
         List[Dict[str, str]]: all documents inside given collection
