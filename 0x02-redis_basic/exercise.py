bf972dcec82d7f156b29e4ec72dbdd48a580bce8
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-17 15:46:05 +0200

    annotate correctly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Cache module
 """
 import redis
-from typing import Any
+from typing import Union
 import uuid
 
 
@@ -15,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
