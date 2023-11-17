8bb57348bc3c7e151013284ca84cdc900f80775d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-17 17:20:17 +0200

    document everything

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,15 +11,17 @@ redis = redis.Redis()
 
 
 def call_history(method: Callable) -> Callable:
+    """
+    Decorator that wraps the get_page function
+    working as a cache manager
+    """
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        func = method.__qualname__
-        url = args[0]
+    def wrapper(url):
         redis.incr(f"count:{url}")
         cache = redis.get(f"cached:{url}")
         if cache:
             return cache.decode()
-        html = method(*args, **kwargs)
+        html = method(url)
         redis.setex(f"cached:{url}", 10, html)
         return html
     return wrapper
@@ -27,5 +29,8 @@ def call_history(method: Callable) -> Callable:
 
 @call_history
 def get_page(url: str) -> str:
+    """
+    make request to a page using request module
+    """
     res = requests.get(url)
     return res.text
