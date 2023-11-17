c8e07c5e5f30a03f2e6d379192d524bf39334764
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-17 15:43:59 +0200

    init Cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Cache module
+"""
+import redis
+from typing import Any
+import uuid
+
+
+class Cache:
+    """
+    Cache class will be used to do operation on redis client
+    """
+
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: any) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
