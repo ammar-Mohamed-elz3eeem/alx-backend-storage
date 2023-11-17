a27c32245f089ad9c2479f5153e1b01f2d4adf6c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-17 16:25:04 +0200

    add function decorators

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,30 @@
 import redis
 from typing import Union, Callable, Optional
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        print(key)
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        inp_key = key + ":inputs"
+        out_key = key + ":outputs"
+        self._redis.rpush(inp_key, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(out_key, str(res))
+        return res
+    return wrapper
 
 
 class Cache:
@@ -11,10 +35,13 @@ class Cache:
     Cache class will be used to do operation on redis client
     """
 
+
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
